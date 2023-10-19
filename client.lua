1d227a43de38c777b0fa4a55f45bda682e09eedd
5M3nd3sSHOP <122824687+5M3nd3sSHOP@users.noreply.github.com>
2023-10-19 23:08:17 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,17 @@
--- Criar uma tabela de localizações com coordenadas
+-- put ur vector3 coords here!
 local locations = {
     { x = -1056.9, y = -861.79, z = 4.94 },
     { x = -1114.59, y = -835.15, z = 13.34 },
     { x = -1111.86, y = -856.03, z = 13.54 },
     { x = -1106.32, y = -822.56, z = 14.28 },
-    -- Adiciona mais localizações conforme necessário
+    -- put ur vector3 coords here!
 }
 
--- Função para criar marcadores em todas as localizações
 function createMarkersForLocations()
     for _, location in ipairs(locations) do
-        local markerType = 20 -- Tipo de marcador (1 é um marcador padrão)
-        local markerScale = vector3(0.5, 0.5, 0.5) -- Escala do marcador
-        local markerColor = {255, 0, 0, 150} -- Cor do marcador (RGBA)
+        local markerType = 20 
+        local markerScale = vector3(0.5, 0.5, 0.5)
+        local markerColor = {255, 0, 0, 150}
 
         Citizen.CreateThread(function()
             local x, y, z = location.x, location.y, location.z
@@ -26,5 +25,5 @@ function createMarkersForLocations()
     end
 end
 
--- Chame a função para criar marcadores para todas as localizações
+
 createMarkersForLocations()
